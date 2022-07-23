61821cb0628cb3d76cc0d14e50be51cd74781763
ztrab <billartzjr@gmail.com>
2022-07-23 11:19:01 -0700

    copy tweaks

--- app/components/MyBio.vue
+++ app/components/MyBio.vue
@@ -11,14 +11,14 @@
       Santa Monica, CA
     </p>
     <p class="mt-4">
-      Hello! I'm Bill and I'm a software engineer and technologist. I primarily build in the front end, specializing in Javascript
-      with the occasional proclivity for back end work. I'm currently employed fulltime with
+      Hello! I'm Bill and I'm a software engineer specializing in the front end
+      with the occasional proclivity for full-stack. I'm currently employed fulltime with
       <a href="https://www.thehutgroup.com" target="_blank" class="text-primary underline">THG</a> working on a bespoke SSR/ISR platform focused
-      on site speed and ease of client integration for headless ecommerce. The platform has launched in beta with a THG owned fashion ecommerce site called
-      <a href="https://beta.www.coggles.com" target="_blank" class="text-primary underline"> Coggles </a>.
+      on site speed, analytics and ease of client integration for headless ecommerce. The platform has launched in beta with a THG owned fashion ecommerce site called
+      <a href="https://beta.www.coggles.com" target="_blank" class="text-primary underline"> Coggles </a>. I previously worked for Target on Dermstore.com which was acquired by THG in 2021.
     </p>
     <p class="mt-3">
-      When I'm not working with my colleagues in the UK making the rectangles glow you can find me hiking, snowboarding in Big Bear or Mammoth, going to concerts or tinkering and/or deep in another
+      When I'm not working with my THG colleagues in the UK making the rectangles glow you can find me hiking or snowboarding in Big Bear and Mammoth, going to concerts, tinkering and/or deep in another
       world in some video game on my PC or Steam Deck. I currently reside in Santa Monica, California.
     </p>
   </div>
