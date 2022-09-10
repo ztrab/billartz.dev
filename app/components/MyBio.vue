c4a6420c83ab612b377db809a896b795a61dded5
ztrab <billartzjr@gmail.com>
2022-09-10 11:19:29 -0700

    copy tweaks

--- app/components/MyBio.vue
+++ app/components/MyBio.vue
@@ -13,8 +13,8 @@
     <p class="mt-4">
       Hello! I'm Bill and I'm a software engineer specializing in front end
       with the occasional proclivity for full-stack. I'm currently employed fulltime with
-      <a href="https://www.thehutgroup.com" target="_blank" class="text-primary underline">THG</a> working on a proprietary SSR/ISR platform focused
-      on site speed, analytics and ease of client integration for headless ecommerce. The platform has launched in beta with a THG owned fashion ecommerce site called
+      <a href="https://www.thehutgroup.com" target="_blank" class="text-primary underline">THG</a> working on a proprietary SSR/Edge SSR platform focused
+      on speed, scalability, analytics and ease of client integration for headless ecommerce. The platform has launched in beta with a THG owned fashion ecommerce site called
       <a href="https://beta.www.coggles.com" target="_blank" class="text-primary underline"> Coggles</a>. I previously worked for Target on Dermstore.com which was acquired by THG in 2021.
     </p>
     <p class="mt-3">
