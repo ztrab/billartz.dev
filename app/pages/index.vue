3b1e841ea1a1c1b1744562a9780d8c0d863877d2
ztrab <billartzjr@gmail.com>
2022-09-11 14:00:00 -0700

    another icon fix attempt

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,8 +3,8 @@
     <div class="flex flex-col justify-between px-10 py-10 lg:py-20 lg:pr-20 xl:pr-0 xl:pl-20 w-full lg:min-w-[400px] lg:max-w-[675px] lg:h-screen">
       <MyBio />
       <div class="mt-20 lg:mt-0 flex flex-row justify-start my-3 text-secondary">
-        <TrendingUpIcon class="w-8 h-8" aria-hidden="true" />
-        <a class="inline-flex items-center transition-all duration-200 ease-in-out group" href="https://www.linkedin.com/in/bill-artz-45a754b/" target="_blank">
+        <a class="group inline-flex items-center transition-all duration-200 ease-in-out" href="https://www.linkedin.com/in/bill-artz-45a754b/" target="_blank">
+          <TrendingUpIcon class="w-10 h-10" aria-hidden="true" />
           <span class="inline-block w-8 bg-primary h-px mx-4 group-hover:w-16 group-hover:bg-secondary transition-all duration-200" />
           <span class="tracking-widest uppercase group-hover:text-primary group-hover:font-bold transition">linkedin</span>
         </a>
