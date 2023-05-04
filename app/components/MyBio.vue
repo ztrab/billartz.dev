bc045fd1ffa5c63689b3503981e36d2bfe6a58a1
Bill Artz <billartzjr@gmail.com>
2023-05-04 11:32:16 -0700

    copy update

--- app/components/MyBio.vue
+++ app/components/MyBio.vue
@@ -4,22 +4,21 @@
       Bill Artz
     </h1>
     <p class="text-2xl mt-1 text-primary font-semibold">
-      Senior Front End Engineer @ THG
+      Senior Front End Engineer
     </p>
     <p>
       <span class="text-sm">Former Target</span><br>
       Santa Monica, CA
     </p>
     <p class="mt-4">
-      Hello! I'm Bill and I'm a software engineer specializing in front end
-      with the occasional proclivity for full-stack. I'm currently employed fulltime with
-      <a href="https://www.thehutgroup.com" target="_blank" class="text-primary font-bold underline">THG</a> working on a proprietary SSR/Edge SSR platform focused
+      Hello! I'm Bill and I'm a full-stack software engineer specializing in front end. I recently worked with
+      <a href="https://www.thehutgroup.com" target="_blank" class="text-primary font-bold underline">THG</a> building a proprietary SSR/Edge SSR platform focused
       on speed, scalability, analytics and ease of client integration for headless ecommerce. The platform is recently out of beta with a THG owned fashion ecommerce site called
-      <a href="https://beta.www.coggles.com" target="_blank" class="text-primary font-bold underline"> Coggles</a>. I previously worked for Target on Dermstore.com which was acquired by THG in 2021.
+      <a href="https://www.coggles.com" target="_blank" class="text-primary font-bold underline"> Coggles</a>. I previously worked for Target on Dermstore.com which was acquired by THG in 2021.
     </p>
     <p class="mt-3">
-      When I'm not working with my THG colleagues in the UK making the rectangles glow you can find me hiking or snowboarding in Big Bear and Mammoth, going to concerts, tinkering and/or deep in another
-      world in some video game on my PC or Steam Deck. I currently reside in Santa Monica, California.
+      When I'm not making the rectangles glow you can find me hiking or snowboarding in Big Bear and Mammoth, going to concerts, tinkering or deep in another
+      world in games on my PC or Steam Deck. I'm currently based in Santa Monica, California.
     </p>
   </div>
 </template>
