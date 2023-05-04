b16c5b3bc4666be9a06c6b7fbe8ae30ad93df49f
Bill Artz <billartzjr@gmail.com>
2023-05-04 11:33:44 -0700

    copy tweak

--- app/components/MyBio.vue
+++ app/components/MyBio.vue
@@ -13,12 +13,12 @@
     <p class="mt-4">
       Hello! I'm Bill and I'm a full-stack software engineer specializing in front end. I recently worked with
       <a href="https://www.thehutgroup.com" target="_blank" class="text-primary font-bold underline">THG</a> building a proprietary SSR/Edge SSR platform focused
-      on speed, scalability, analytics and ease of client integration for headless ecommerce. The platform is recently out of beta with a THG owned fashion ecommerce site called
+      on speed, scalability, analytics and ease of client integration for headless composable ecommerce. The platform is recently out of beta with a THG owned fashion ecommerce site called
       <a href="https://www.coggles.com" target="_blank" class="text-primary font-bold underline"> Coggles</a>. I previously worked for Target on Dermstore.com which was acquired by THG in 2021.
     </p>
     <p class="mt-3">
       When I'm not making the rectangles glow you can find me hiking or snowboarding in Big Bear and Mammoth, going to concerts, tinkering or deep in another
-      world in games on my PC or Steam Deck. I'm currently based in Santa Monica, California.
+      world in various games on my PC or Steam Deck. I'm currently based in Santa Monica, California.
     </p>
   </div>
 </template>
